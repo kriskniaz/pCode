dcddee59ce8a5531e4b74712db98e6775db93cbd
Kris Kniaz <kris@kniaz.net>
2025-07-14 09:40:51 -0400

    update ri matrics values

--- AHP/AHP/Constants.cs
+++ AHP/AHP/Constants.cs
@@ -2,16 +2,36 @@ using System;
 
 namespace Net.Kniaz.AHP
 {
-	/// <summary>
-	/// Random Indices values (Consistency Indices for randomly selected values
-	/// in the priority matrix) taken from Saaty.
-	/// </summary>
-	public class Constants
-	{
-		//values are for the following dimensions of the requirements matrix
-		//0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
-		public static double[]randomIndices = {0.00, 0.00, 0.00, 0.58, 0.90, 1.12,
-										1.24, 1.32, 1.41, 1.45,	1.49, 
-										1.51, 1.48, 1.56, 1.57, 1.59, 1.605, 1.61, 1.615, 1.62, 1.625};
-	}
+    /// <summary>
+    /// Random Indices values (Consistency Indices for randomly selected values
+    /// in the priority matrix) taken from Saaty.
+    /// </summary>
+    public class Constants
+    {
+        // values are for the following dimensions of the requirements matrix
+        // 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+        public static double[] randomIndices = {
+        0.00,  // 0
+        0.00,  // 1
+        0.00,  // 2
+        0.58,  // 3
+        0.90,  // 4
+        1.12,  // 5
+        1.24,  // 6
+        1.32,  // 7
+        1.41,  // 8
+        1.45,  // 9
+        1.49,  // 10
+        1.51,  // 11
+        1.48,  // 12
+        1.56,  // 13
+        1.57,  // 14
+        1.58,  // 15
+        1.59,  // 16
+        1.60,  // 17
+        1.61,  // 18
+        1.62,  // 19
+        1.63   // 20
+    };
+}
 }
